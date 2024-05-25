87b1cfb896e1e53bc8e19a5f5b0c6d013e1d85e9
A7Y_Chris <tristan.aly@gmail.com>
2024-05-25 09:44:21 +0200

    reset the form to add projects
    added resetForm , handleBack handleClose functions to clear the form when it closes or returns

--- Frontend/src/components/modales/addPicture.jsx
+++ Frontend/src/components/modales/addPicture.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { postWork } from '../../services/api';
+import React, { useState, useEffect } from "react";
+import { postWork } from "../../services/api";
 
 function AddPicture({ isOpen, onClose, onBack, categories, refreshPictures }) {
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const [image, setImage] = useState(null);
-  const [categoryId, setCategoryId] = useState('');
-  const [preview, setPreview] = useState('');
+  const [categoryId, setCategoryId] = useState("");
+  const [preview, setPreview] = useState("");
 
   useEffect(() => {
     console.log("AddPicture modal isOpen:", isOpen);
@@ -26,9 +26,9 @@ function AddPicture({ isOpen, onClose, onBack, categories, refreshPictures }) {
       return;
     }
     const formData = new FormData();
-    formData.append('title', title);
-    formData.append('image', image);
-    formData.append('category', categoryId);
+    formData.append("title", title);
+    formData.append("image", image);
+    formData.append("category", categoryId);
 
     console.log("Submitting formData:", {
       title,
@@ -36,49 +36,98 @@ function AddPicture({ isOpen, onClose, onBack, categories, refreshPictures }) {
       categoryId,
     });
 
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const response = await postWork(formData, token);
     if (response) {
       refreshPictures();
-      onClose();
+      handleClose();
     }
   };
+  // Fonction pour réinitialiser les états du formulaire
+  const resetForm = () => {
+    setTitle("");
+    setImage(null);
+    setCategoryId("");
+    setPreview("");
+  };
 
+  // Handlers pour fermer ou revenir en arrière, en réinitialisant le formulaire
+  const handleClose = () => {
+    resetForm();
+    onClose();
+  };
+
+  const handleBack = () => {
+    resetForm();
+    onBack();
+  };
   if (!isOpen) return null;
 
   return (
     <div className="modal addPicture">
       <div className="modal-content">
         <div className="navigation">
-          <button type="button" onClick={onBack}><i className="fa-solid fa-arrow-left"></i></button>
-          <button type="button" onClick={onClose}>x</button>
+          <button type="button" onClick={handleBack}>
+            <i className="fa-solid fa-arrow-left"></i>
+          </button>
+          <button type="button" onClick={handleClose}>
+            x
+          </button>
         </div>
         <h2>Ajout photo</h2>
         {preview ? (
           <div className="file-upload-container">
-            <img src={preview} alt="Preview" className="preview-image"/>
+            <img src={preview} alt="Preview" className="preview-image" />
           </div>
         ) : (
           <div className="file-upload-container">
             <i className="fa-solid fa-image"></i>
             <div className="styled-upload-button-wrapper">
-              <button type="button" className="styled-upload-button">Ajouter une photo</button>
-              <input type="file" id="file-upload" className="file-upload-input" onChange={handleImageChange} required />
+              <button type="button" className="styled-upload-button">
+                Ajouter une photo
+              </button>
+              <input
+                type="file"
+                id="file-upload"
+                className="file-upload-input"
+                onChange={handleImageChange}
+                required
+              />
             </div>
             <p className="uploadDetails">Taille max: 5Mo</p>
           </div>
         )}
         <form onSubmit={handleSubmit}>
           <label htmlFor="title">Titre</label>
-          <input type="text" value={title} onChange={e => setTitle(e.target.value)} required />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
           <label htmlFor="category">Catégorie</label>
-          <select value={categoryId} onChange={e => setCategoryId(e.target.value)} required>
-            <option value="" disabled>Choisir une catégorie</option>
-            {categories.map(category => (
-              <option key={category.id} value={category.id}>{category.name}</option>
+          <select
+            value={categoryId}
+            onChange={(e) => setCategoryId(e.target.value)}
+            required
+          >
+            <option value="" disabled>
+              Choisir une catégorie
+            </option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
             ))}
           </select>
-          <button type="submit" className={`submit-button ${title && image && categoryId ? 'active' : ''}`}>Valider</button>
+          <button
+            type="submit"
+            className={`submit-button ${
+              title && image && categoryId ? "active" : ""
+            }`}
+          >
+            Valider
+          </button>
         </form>
       </div>
     </div>
